00e3dc98cab0b33bdb825df57150ff8214d79f30
OlivierLutz <olivierlutz8@gmail.com>
2020-08-02 15:57:26 +0200

    Ajout anagramme

---
+++ anagramme.rb
@@ -0,0 +1,19 @@
+#!C:/Ruby27-x64/bin/ruby -w
+system "clear"
+
+lines = Array.new
+File.open(ARGV[1]).each { |line| lines << line.chomp }
+
+b = 0
+somme = 0
+somme_mot = 0
+ARGV[0].each_byte {|x| somme_mot += x}
+
+while b < lines.size
+	lines[b].each_byte {|a| somme += a}
+	if somme == somme_mot
+		puts lines[b]
+	end
+	somme = 0
+	b +=1
+end
